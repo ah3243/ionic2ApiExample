13fa2f35d8d3d04bc632cae31f45500df57158d6
Albert Higgins <1c2fffe2@opayq.com>
2016-10-29 15:34:28 +0100

    created method in home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { PeopleSearch } from '../../providers/people-search';
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [PeopleSearch]
 })
 export class HomePage {
+  public people: any;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public peopleSearch: PeopleSearch) {
+    this.loadPeople();
   }
 
+  loadPeople() {
+    this.peopleSearch.load()
+      .then(data => { 
+        this.people = data;
+      });
+  }
 }
